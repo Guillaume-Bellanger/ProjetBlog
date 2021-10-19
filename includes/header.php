643ceeac8b88929df1642054c51d802e6a08edaf
Guillaume B <bellanger.guillaume77@gmail.com>
2021-10-19 21:03:23 +0200

    first commit

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<header>
+    <div class="logo">Dyma Blog</div>
+    <ul class="header-menu">
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/add-article.php' ? 'active' : '' ?>>
+        <a href="/add-article.php">Ecrire un article</a>
+    </ul>
+</header>
